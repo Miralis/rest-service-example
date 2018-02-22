453b64d5874acaef22d2904e47e7c1a0026e6f02
Gredler <an.gredler@hotmail.com>
2018-02-22 14:37:54 +0100

    implemented bootstrap

--- src/main/java/com/example/Bootstrap/Bootstrap.java
+++ src/main/java/com/example/Bootstrap/Bootstrap.java
@@ -1,5 +1,42 @@
 package com.example.Bootstrap;
 
 
-public class Bootstrap {
+import com.example.domain.Person;
+import com.example.repositories.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner{
+
+    private final PersonRepository personRepository;
+
+    public Bootstrap(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        loadPersons();
+    }
+
+    private void loadPersons() {
+        Person person1 = new Person();
+        person1.setId(1L);
+        person1.setFirstName("John");
+        person1.setLastName("Cena");
+        personRepository.save(person1);
+
+        Person person2 = new Person();
+        person2.setId(2L);
+        person2.setFirstName("Sarah");
+        person2.setLastName("Conner");
+        personRepository.save(person2);
+
+        Person person3 = new Person();
+        person3.setId(3L);
+        person3.setFirstName("Testi");
+        person3.setLastName("Test");
+        personRepository.save(person3);
+    }
 }
